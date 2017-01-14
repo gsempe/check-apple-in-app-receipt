bf3fd5fe289ca138b2118969f8e6cb2fadbfcbfa
Guillaume Sempe <guillaume.sempe@gmail.com>
2017-01-14 23:04:47 +0000

    Add status code meanings to help

--- check-receipt.sh
+++ check-receipt.sh
@@ -5,7 +5,15 @@ function usage
 {
     echo "usage: $0 -r receipt-file -p password [--sandbox] | -h"
     echo ""
-    echo "Examples:"
+    echo "Status codes:"
+    echo "21000 The App Store could not read the JSON object you provided."
+    echo "21002 The data in the receipt-data property was malformed or missing."
+    echo "21003 The receipt could not be authenticated."
+    echo "21004 The shared secret you provided does not match the shared secret on file for your account. Only returned for iOS 6 style transaction receipts for auto-renewable subscriptions."
+    echo "21005 The receipt server is not currently available."
+    echo "21006 This receipt is valid but the subscription has expired. When this status code is returned to your server, the receipt data is also decoded and returned as part of the response. Only returned for iOS 6 style transaction receipts for auto-renewable subscriptions."
+    echo "21007 This receipt is from the test environment, but it was sent to the production environment for verification. Send it to the test environment instead."
+    echo "21008 This receipt is from the production environment, but it was sent to the test environment for verification. Send it to the production environment instead."
 }
 
 receiptfile=
